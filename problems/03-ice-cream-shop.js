963fa5bdd567d12551abb3b0ff7b833d6676955b
Nathan Mount <nathanmount10@gmail.com>
2021-02-23 10:31:46 -0800

    finished 03

--- problems/03-ice-cream-shop.js
+++ problems/03-ice-cream-shop.js
@@ -5,16 +5,26 @@ user's favorite ice cream flavor. Recursively find out whether or not the shop
 offers their favorite flavor.
 
 Examples:
-iceCreamShop(['vanilla', 'strawberry'], 'blue moon'); // false
-iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea'); // true
-iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio'); // false
-iceCreamShop(['moose tracks'], 'moose tracks'); // true
-iceCreamShop([], 'honey lavender'); // false
-***********************************************************************/
 
+***********************************************************************/
+/*
+  base case: if (flavors.length === 0) return;
+  recursive case: if (flavors[0] === favorite) return true;
+*/
 
 // your code here
+function iceCreamShop(flavors, favorite) {
+  if (flavors.length === 0) return false;
+  if (flavors[0] === favorite) return true;
+  return iceCreamShop(flavors.slice(1), favorite);
+}
 
+
+console.log(iceCreamShop(['vanilla', 'strawberry'], 'blue moon')); // false
+console.log(iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')); // true
+console.log(iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')); // false
+console.log(iceCreamShop(['moose tracks'], 'moose tracks')); // true
+console.log(iceCreamShop([], 'honey lavender')); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
