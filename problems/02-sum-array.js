db2df2004728103b0d4087aeca7b79c2ae69cdaa
Rebecca <rebeccawchen@gmail.com>
2021-02-23 12:23:07 -0600

    problem 2 done

--- problems/02-sum-array.js
+++ problems/02-sum-array.js
@@ -8,10 +8,20 @@ Examples:
 sumArray([1, 2, 3]); //  6
 sumArray([0, 1, -3]); //  -2
 sumArray([1, 2, 3, 4, 5]); // 15
+
+base case: array.length === 0
+recursive case : array[0] + array.slice(1);
 ***********************************************************************/
 
-// your code here
-  
+const sumArray = integers => {
+  if (integers.length === 0) return 0;
+
+  return integers[0] + sumArray(integers.slice(1));
+}
+
+console.log(sumArray([1, 2, 3])); //  6
+console.log(sumArray([0, 1, -3])); //  -2
+console.log(sumArray([1, 2, 3, 4, 5])); // 15
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = sumArray;
