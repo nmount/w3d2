73b8c89e134fb87d55e409a7afe5cc9222917e79
Rebecca <rebeccawchen@gmail.com>
2021-02-23 12:54:53 -0600

    problem 4 done

--- problems/04-range.js
+++ problems/04-range.js
@@ -11,9 +11,17 @@ range(7, 6); // []
 ***********************************************************************/
 
 
-// your code here
+const range = (start, end) => {
+  debugger;
+  if (end <= start) return [];
+  // return [start].concat(range(start + 1, end));
+  return [start, ...range(start + 1, end)];
 
+}
 
+console.log(range(1, 5)); // [1, 2, 3, 4]
+console.log(range(3, 4)); // [3]
+console.log(range(7, 6)); // []
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = range;
