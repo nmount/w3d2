d556f5e4ffa7c5423ff6011ea96e5523601b505a
Nathan Mount <nathanmount10@gmail.com>
2021-02-23 13:37:33 -0800

    finished 09

--- problems/09-fibonacci.js
+++ problems/09-fibonacci.js
@@ -12,19 +12,31 @@ examples below if you're unsure where to start!
 
 Examples:
 
-fibonacci(1); // 1
-fibonacci(2); // 1
-fibonacci(3); // 2
-fibonacci(4); // 3
-fibonacci(10); // 55
+
+
+
+fibonacci(11); // 55 + fibonacci(11-1) + fibonacci(11-2) //
+
+
 ***********************************************************************/
 
 // your code here
-  
+function fibonacci(n) {
+  if (n < 2) return n;
+
+  return fibonacci(n-1) + fibonacci(n-2);
+
+}
+
+console.log(fibonacci(1)); // 1
+console.log(fibonacci(2)); // 1
+console.log(fibonacci(3)); // 2
+console.log(fibonacci(4)); // 3
+console.log(fibonacci(10)); // 55
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = fibonacci;
 } catch (e) {
   module.exports = null;
 }
-  
