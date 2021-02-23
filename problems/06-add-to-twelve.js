50e2bb12f07d1a9f4c1eeeb8e90585d80d000cc1
Rebecca <rebeccawchen@gmail.com>
2021-02-23 14:41:16 -0600

    problem 6 done

--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -10,10 +10,25 @@ addToTwelve([1, 3, 4, 7, 6]); // false
 addToTwelve([1, 11, 4, 7, 6]); // true
 addToTwelve([1, 12, 4, 7, 6]); // false
 addToTwelve([1]); // false
+
+base case: if array.length < 2 return false
 ***********************************************************************/
 
-// your code here
+const addToTwelve = numbers => {
+  if (numbers.length < 2) return false;
 
+  if (numbers[numbers.length - 1] + numbers[numbers.length - 2] === 12) return true;
+
+  numbers.pop();
+  return addToTwelve(numbers);
+
+
+  // numbers = [1, 3, 4, 7]
+}
+console.log(addToTwelve([1, 3, 4, 7, 5])); // true)
+console.log(addToTwelve([1, 3, 4, 7, 6])); // false
+console.log(addToTwelve([1, 11, 4, 7, 6])); //true 
+console.log(addToTwelve([1, 12, 4, 7, 6])); //false
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = addToTwelve;
