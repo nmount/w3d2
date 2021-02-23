151de68b5ed7d607aff49d0cc574d20be3cd63f7
Nathan Mount <nathanmount10@gmail.com>
2021-02-23 12:57:15 -0800

    finished 07

--- problems/07-exponent.js
+++ problems/07-exponent.js
@@ -1,5 +1,5 @@
 /***********************************************************************
-Write a recursive function called `exponent` that takes two integers, 
+Write a recursive function called `exponent` that takes two integers,
 `num` and `power`, and returns `num` raised to the `power`th power. Your
 function should work when `num` or `power` are positive OR negative.
 
@@ -15,15 +15,20 @@ of the numerator:
 
 Examples:
 
-exponent(3, 2); // 9
-exponent(2, -2); // 1/4 (or 0.25)
-exponent(5, 5); // 3125
+
 ***********************************************************************/
 
 function exponent(b, n) {
   // your code here
+  if (n === 0) return 1;
+  if (n < 0) return 1/b * exponent(b, n + 1);
+  return b * exponent(b, n - 1);
 }
-  
+
+
+console.log(exponent(3, 2)); // 9
+console.log(exponent(2, -2)); // 1/4 (or 0.25)
+console.log(exponent(5, 5)); // 3125
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = exponent;
